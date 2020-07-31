4411cfbff6211d94c35816b6744709d7bf629b56
sander <s.teunissen@gmail.com>
2020-07-31 10:59:47 +0200

    update coverage report.

--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ def install_flit_dev_deps(session):
 @nox.session(python=["3.7", "3.8"])
 def tests(session):
     install_flit_dev_deps(session)
-    session.run("pytest", "--cov=aio_wx_widgets", "--cov-report=html", "tests")
+    session.run("pytest", "--cov=aio_wx_widgets", "--cov-report=xml:cov.xml", "tests")
 
 
 @nox.session(python=["3.7"])
